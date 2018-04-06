40977a423353bfcb563999ae40e6442add4aaddf
Nicolas Vidal <nicolas.vidal@iscool-e.com>
2018-04-06 12:06:36 +0200

    We can now interrogate the Oracle !

--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -6,10 +6,10 @@ public class MainScript : MonoBehaviour
 	public Transform[] cubes;
 	
 	// Use this for initialization
-	void Start () {
-		Debug.Log("Hello les 3A AL1 !!! :)"); 
-		Debug.Log(cubes.Length);
-		StartCoroutine(Scramble(cubes));
+	IEnumerator Start () {
+		Debug.Log("Avant Mélange : " + GetError(cubes));
+		yield return StartCoroutine(Scramble(cubes));
+		Debug.Log("Après Mélange : " + GetError(cubes));
 	}
 
 	IEnumerator Scramble(Transform[] cubes)
@@ -27,5 +27,23 @@ public class MainScript : MonoBehaviour
 		var tmp = cube1.position;
 		cube1.position = cube2.position;
 		cube2.position = tmp;
+	}
+
+	int GetError(Transform[] cubes)
+	{
+		var error = 0;
+
+		foreach (var cube1 in cubes)
+		{
+			foreach (var cube2 in cubes)
+			{
+				if (cube1.tag == cube2.tag && cube1.position.y != cube2.position.y)
+				{
+					error++;
+				}
+			}
+		}
+
+		return error;
 	}
 }
