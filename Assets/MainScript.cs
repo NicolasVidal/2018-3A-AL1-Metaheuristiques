55c0c68ffcf424e6374d67209b9eb07aade83c3c
Nicolas Vidal <nicolas.vidal@iscool-e.com>
2018-04-06 11:17:07 +0200

    Hello World and cube referencing

---
+++ Assets/MainScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+	public Transform[] cubes;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello les 3A AL1 !!! :)"); 
+		Debug.Log(cubes.Length); 
+	}
+}
